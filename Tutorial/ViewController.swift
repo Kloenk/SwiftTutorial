c835f2cd922de53b9969728a8cdf4dc3f6ff54bf
Finn Behrens <kloenk@gmx.de>
2017-06-16 08:54:58 +0200

    Added delegate to text input box and sett text from input box to label

--- Tutorial/ViewController.swift
+++ Tutorial/ViewController.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
-    @IBOutlet weak var NameTextField: UIStackView!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var TextInputField: UITextField!
     
     //MARK: Actions
     
     @IBAction func setDefaultLabelText(_ sender: Any) {
-        mealNameLabel.text = "Hallo Welt!"
+        setTextFildd(TextInputField, mealNameLabel)
+        TextInputField.resignFirstResponder()
+        
+    }
+    
+    //MARK: text field
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //mealNameLabel.text = textField.text
+        setTextFildd(textField, mealNameLabel)
+    }
+    
+    //MARK: own functions
+    func setTextFildd(_ textFiled: UITextField, _ labe: UILabel) {
+        if textFiled.text?.count == 0 {
+            labe.text = "Bitte setze einen Text"
+        } else {
+            labe.text = textFiled.text
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TextInputField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
